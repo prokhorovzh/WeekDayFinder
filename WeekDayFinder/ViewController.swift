69f9a2fdab801c5e1bf99310b5f44bc386989c2a
Eugene <prokhorov.eu@icloud.com>
2020-02-07 11:48:05 +0400

    Initial Commit

---
+++ WeekDayFinder/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WeekDayFinder
+//
+//  Created by Евгений Прохоров on 07.02.2020.
+//  Copyright © 2020 Евгений Прохоров. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
